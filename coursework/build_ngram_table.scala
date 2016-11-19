d04a810195f80d4e4037fe7ea618b78ae2390a10
Chris Snow <chsnow123@gmail.com>
2016-11-19 19:56:42 +0000

    Update build_ngram_table.scala

--- coursework/build_ngram_table.scala
+++ coursework/build_ngram_table.scala
@@ -2,14 +2,16 @@ import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer}
 import org.apache.spark.ml.feature.NGram
 import org.apache.spark.sql.functions.countDistinct
 
-val docDataFrame = sc.textFile("en_US.all.txt").toDF("document")
+val docDataFrame = sc.textFile("en_US.all.txt").toDF("document")//.limit(10000)
 
 val regexTokenizer = new RegexTokenizer().
                             setInputCol("document").
                             setOutputCol("words").
                             setPattern("\\W") 
 
-val tokenizedDoc = regexTokenizer.transform(docDataFrame)
+val tokenizer = new Tokenizer().setInputCol("document").setOutputCol("words")
+
+val tokenizedDoc = tokenizer.transform(docDataFrame)
 
 val ngram = new NGram().setInputCol("words").setOutputCol("ngrams")
 val ngramDataFrame = ngram.transform(tokenizedDoc)
@@ -21,7 +23,21 @@ val ngramsOnlyDF = ngramDataFrame.
         map(item => item.swap).
         filter(ngram => ngram._2 > 10)
 
+val ngramsCount = ngramDataFrame.
+        flatMap(_.getAs[Stream[String]]("ngrams").toList).
+        map( ngram => (ngram, 1)).reduceByKey(_ + _).
+        count()
+
+object MyFunctions {
+  def func1(t: Tuple2[String, Int]): String = { 
+    val parts = t._1.split(" ")
+    return parts(0) + "," + parts(1) + "," + t._2.toDouble / ngramsCount.toDouble
+  }
+}
+
 ngramsOnlyDF.
-    map(x => s"${x._1},${x._2}").
+    map(MyFunctions.func1).
     coalesce(1).
     saveAsTextFile("digrams.csv")
+
+System.exit(0)
