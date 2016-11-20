0d4e86d7cd60421ac3ffcf8d646d250f1ae662f2
christopher snow <chsnow123@gmail.com>
2016-11-20 12:18:56 +0000

    verify input text is not empty

--- coursework/Text_Predictor/server.R
+++ coursework/Text_Predictor/server.R
@@ -6,11 +6,16 @@ digrams <- read.csv('./digrams.csv', header = FALSE, sep = '\t')
 shinyServer(function(input, output) {
  
   getPredictions <- reactive({
-    last_word <- word(input$input_text,-1)
-    found_diagrams <- digrams[ digrams$V1 == last_word, ]
-    top_five_digrams <- found_diagrams[ order(found_diagrams$V3, decreasing = TRUE), ][1:5, 2]
-    top_five_digrams <- as.character(top_five_digrams)
-    top_five_digrams <- paste(top_five_digrams, collapse =" : ")
+    if (nchar(input$input_text) > 0) {
+      last_word <- word(input$input_text,-1)
+      found_diagrams <- digrams[ digrams$V1 == last_word, ]
+      top_five_digrams <- found_diagrams[ order(found_diagrams$V3, decreasing = TRUE), ][1:5, 2]
+      top_five_digrams <- as.character(top_five_digrams)
+      top_five_digrams <- paste(top_five_digrams, collapse =" : ")
+      return (top_five_digrams)
+    } else {
+      return ("")
+    }
   })
    
   output$output_text <- renderText({
